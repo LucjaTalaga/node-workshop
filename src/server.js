6e46c4cc12af1205eb7e7b2fbadfe967edcd69f7
Piotr Talaga <piotrek.talaga@gmail.com>
2020-03-25 13:05:12 +0100

    adding response from server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 
 const http = require('http');
-const server = http.createServer();
+const server = http.createServer(handler);
+const message = 'I am so happy to be part of the Node Girls workshop!';
+
+function handler (request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(message);
+    response.end();
+}
+
 
 server.listen(3000, function () {
 
